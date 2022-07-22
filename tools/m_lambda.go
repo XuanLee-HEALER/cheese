c7c3dd262c026bb059e0db63221daea0610e02fa
mouselee <mouse.tree.lee@gmail.com>
2022-07-22 18:38:12 +0800

    修改Map函数定义

--- tools/m_lambda.go
+++ tools/m_lambda.go
@@ -1,13 +1,27 @@
 package tools
 
-func MapToStr(o any, f func(any) string) string {
-	return f(o)
-}
-
-func MapToStrArr(os []any, f func(any) string) []string {
-	res := []string{}
+func MapTo(os []any, f func(any) any) []any {
+	res := make([]any, 0)
 	for _, o := range os {
 		res = append(res, f(o))
 	}
 	return res
+}
+
+func MapToString(os []any) []string {
+	res := make([]string, 0)
+	for _, o := range os {
+		res = append(res, o.(string))
+	}
+	return res
+}
+
+func Filter(os []any, f func(any) bool) []any {
+	cp := make([]any, 0)
+	for _, o := range os {
+		if f(o) {
+			cp = append(cp, o)
+		}
+	}
+	return cp
 }
